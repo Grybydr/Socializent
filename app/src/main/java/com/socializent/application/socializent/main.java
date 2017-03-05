d913cd73d2fb0960963dc3f852e3bf2c80d31d0b
grybydr <guraybaydur@gmail.com>
2017-03-05 21:42:56 +0300

    loginPageAddedv2

--- app/src/main/java/com/socializent/application/socializent/main.java
+++ app/src/main/java/com/socializent/application/socializent/main.java
@@ -3,6 +3,7 @@ package com.socializent.application.socializent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -14,6 +15,7 @@ public class main extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
 
         Button loginButton = (Button)findViewById(R.id.loginButton);
