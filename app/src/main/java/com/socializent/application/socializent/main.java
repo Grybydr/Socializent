e4e3e455aa4119d65762623b9eea0a3fde27e53c
zulalB <zulal.121@gmail.com>
2017-03-05 19:04:24 +0300

    loginPage_V1

---
+++ app/src/main/java/com/socializent/application/socializent/main.java
@@ -0,0 +1,23 @@
+package com.socializent.application.socializent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by Zulal Bingol on 5.03.2017.
+ */
+
+public class main extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Button loginButton = (Button)findViewById(R.id.loginButton);
+        EditText userNameText = (EditText)findViewById(R.id.userNameText);
+        EditText passwordText = (EditText)findViewById(R.id.passwordText);
+    }
+}
