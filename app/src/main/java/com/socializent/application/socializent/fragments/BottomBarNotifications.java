f85b8a1fc11ea10a5b1236746d5e0243e44bdf30
grybydr <guraybaydur@gmail.com>
2017-03-14 00:10:43 +0300

    bottombarchanged

--- app/src/main/java/com/socializent/application/socializent/fragments/BottomBarNotifications.java
+++ app/src/main/java/com/socializent/application/socializent/fragments/BottomBarNotifications.java
@@ -1,8 +1,31 @@
 package com.socializent.application.socializent.fragments;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.socializent.application.socializent.R;
+
 /**
  * Created by Irem on 13.3.2017.
  */
 
-public class BottomBarNotifications {
+public class BottomBarNotifications  extends Fragment {
+    public static BottomBarMap newInstance() {
+        BottomBarMap fragment = new BottomBarMap();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.bottom_bar_notification_fragment, container, false);
+    }
 }
