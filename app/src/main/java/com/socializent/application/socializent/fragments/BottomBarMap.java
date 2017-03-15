0312ee0a3a5e46190e61f3829817691d0edb0bb0
zulalB <zulal.121@gmail.com>
2017-03-15 19:15:19 +0300

    BottomBarMap yapıldı

--- app/src/main/java/com/socializent/application/socializent/fragments/BottomBarMap.java
+++ app/src/main/java/com/socializent/application/socializent/fragments/BottomBarMap.java
@@ -4,15 +4,36 @@ package com.socializent.application.socializent.fragments;
  * Created by Irem on 13.3.2017.
  */
 
-
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.socializent.application.socializent.R;
 
-public class BottomBarMap extends Fragment {
+public class BottomBarMap extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private GoogleMap myGoogleMap;
+    private UiSettings myUiSettings;
+    FloatingActionButton myFabButton;
+    private LatLng eventPoint;
+
     public static BottomBarMap newInstance() {
         BottomBarMap fragment = new BottomBarMap();
         return fragment;
@@ -26,6 +47,102 @@ public class BottomBarMap extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.bottom_bar_map_fragment, container, false);
+        View mapView = inflater.inflate(R.layout.bottom_bar_map_fragment, container, false);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapView);  //---------CHECK!
+
+        mapFragment.getMapAsync(this);
+
+        myFabButton = (FloatingActionButton) mapView.findViewById(R.id.addEventFab);
+        myFabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setUpMap();
+            }
+        });
+
+        Toast.makeText(getContext(), R.string.map_gps_warning, Toast.LENGTH_LONG).show();
+
+        return mapView;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        myGoogleMap = googleMap;
+        myUiSettings = myGoogleMap.getUiSettings();
+        myUiSettings.setZoomControlsEnabled(true);
+        myUiSettings.setCompassEnabled(true);
+        myUiSettings.setZoomGesturesEnabled(true);
+        myUiSettings.setScrollGesturesEnabled(true);
+
+        updateMyLocation();
+    }
+
+    private void setUpMap() {
+        myGoogleMap.setOnMapClickListener(this);
+    }
+
+    @Override
+    public void onMapClick(LatLng point) {
+        eventPoint = point;
+        addEvent(eventPoint);
+    }
+
+    private void addEvent(LatLng point) {
+        myGoogleMap.addMarker(new MarkerOptions().position(point).title("Event"));
+    }
+
+    private boolean checkMapReady() {
+        if (myGoogleMap == null) {
+            Toast.makeText(getContext(), R.string.map_not_ready, Toast.LENGTH_SHORT).show();//------CHECK
+            return false;
+        }
+        return true;
+    }
+
+    private void updateMyLocation() {
+        Log.v("LOCATION", "updateMyLocation");
+        if (!checkMapReady()) {
+            return;
+        }
+        if (ActivityCompat.checkSelfPermission(getActivity().getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity().getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.v("LOCATION", "Permissions GIVEN for My Location");
+            myGoogleMap.setMyLocationEnabled(true);
+            myGoogleMap.setBuildingsEnabled(true);
+            return;
+        }
+        else {
+            PermissionUtils.requestPermission((AppCompatActivity) getActivity(), LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+            Log.v("LOCATION", "Permission conditions not satisfied for My Location");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        myGoogleMap.setMyLocationEnabled(true);
     }
 }
