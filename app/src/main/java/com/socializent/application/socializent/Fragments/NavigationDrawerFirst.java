eaab9d8ba53096b36d5ae6f273f6e42de6fb5c99
grybydr <guraybaydur@gmail.com>
2017-03-20 01:51:46 +0300

    circular image added to profile

--- app/src/main/java/com/socializent/application/socializent/Fragments/NavigationDrawerFirst.java
+++ app/src/main/java/com/socializent/application/socializent/Fragments/NavigationDrawerFirst.java
@@ -1,10 +1,13 @@
 package com.socializent.application.socializent.Fragments;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,11 +38,16 @@ public class NavigationDrawerFirst extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         profileView = inflater.inflate(R.layout.material_design_profile_screen_xml_ui_design, container, false);
-        ImageView image = (ImageView) profileView.findViewById(R.id.user_profile_photo);
-        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.id.user_profile_photo);
+        //ImageView image = (ImageView) profileView.findViewById(R.id.user_profile_photo);
+        //Bitmap bm = BitmapFactory.decodeResource(getResources(), R.id.user_profile_photo);
         //image.setImageBitmap(bm);
-        circle = new CircleDrawable(bm,true);
-        image.setBackground(circle);
+        //circle = new CircleDrawable(bm,true);
+        //image.setBackground(circle);
+        //Bitmap icon = BitmapFactory.decodeResource(getResources(),
+                //R.drawable.lala);
+       //RoundedBitmapDrawable dr = RoundedBitmapDrawableFactory.create(getResources(),icon);
+       // dr.setCornerRadius(new Float(100));
+       // image.setImageDrawable(dr);
 
         return profileView;
 
