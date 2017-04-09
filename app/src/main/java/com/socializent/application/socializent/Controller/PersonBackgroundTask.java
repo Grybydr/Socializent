b9eec52c644d8756a3665ee23fe7877a8b22190b
grybydr <guraybaydur@gmail.com>
2017-04-09 17:24:36 +0300

    PersonBackgroundTask added

---
+++ app/src/main/java/com/socializent/application/socializent/Controller/PersonBackgroundTask.java
@@ -0,0 +1,14 @@
+package com.socializent.application.socializent.Controller;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Toshıba on 04/09/2017.
+ */
+
+public class PersonBackgroundTask extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] params) {
+        return null;
+    }
+}
