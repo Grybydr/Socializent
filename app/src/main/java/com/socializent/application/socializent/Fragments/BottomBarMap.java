7c7413a04c308dc3c20609d3197ecb6988b19c92
zulalB <zulal.121@gmail.com>
2017-04-11 22:54:06 +0300

    eventCreateDialog, placepicker added

--- app/src/main/java/com/socializent/application/socializent/Fragments/BottomBarMap.java
+++ app/src/main/java/com/socializent/application/socializent/Fragments/BottomBarMap.java
@@ -2,18 +2,22 @@ package com.socializent.application.socializent.Fragments;
 
 /**
  * Created by Irem on 13.3.2017.
+ * Edited by Zulal :)
  */
 
 import android.Manifest;
-import android.app.Activity;
+import android.support.v4.app.DialogFragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,40 +25,42 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.socializent.application.socializent.R;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
-
-import static android.app.Activity.RESULT_OK;
-import static junit.framework.Assert.assertEquals;
-
-public class BottomBarMap extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+public class BottomBarMap extends Fragment implements OnMapReadyCallback, LocationListener,  DialogInterface.OnCancelListener, DialogInterface.OnDismissListener {
 
     //CONSTANTS
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private static final int REQUEST_PLACE_PICKER = 1;
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final int DEFAULT_ZOOM = 17;
+    private static final int DEFAULT_BEARING = 90;
+    private static final int DEFAULT_TILT = 0;
+    private static final long MIN_TIME = 400;
+    private static final float MIN_DISTANCE = 1000;
+
+    private final LatLng defaultLocation = new LatLng(39.868010, 32.748823); //Bilkent's Coordinates
 
     //VARIABLES
     private GoogleMap myGoogleMap;
     private UiSettings myUiSettings;
     FloatingActionButton myFabButton;
-    private LatLng eventPoint;
-    private Geocoder geocoder;
-    List<Address> addresses;
+    private LocationManager locationManager;
+    private boolean showDialog = false;
 
     public static BottomBarMap newInstance() {
         BottomBarMap fragment = new BottomBarMap();
@@ -64,6 +70,7 @@ public class BottomBarMap extends Fragment implements OnMapReadyCallback, Google
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        locationManager = (LocationManager) getActivity().getSystemService(getContext().LOCATION_SERVICE);
     }
 
     @Override
@@ -72,23 +79,33 @@ public class BottomBarMap extends Fragment implements OnMapReadyCallback, Google
         View mapView = inflater.inflate(R.layout.bottom_bar_map_fragment, container, false);
 
         SupportMapFragment mapFragment =
-                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapView);  //---------CHECK!
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapView);
 
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            PermissionUtils.requestPermission((AppCompatActivity) getActivity(), LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+            Log.v("LOCATION", "Permission conditions not satisfied for My Location");
+
+            Toast.makeText(getContext(), R.string.map_loc_warning, Toast.LENGTH_LONG).show();
+            myGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
+        }
+        //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
         mapFragment.getMapAsync(this);
 
         myFabButton = (FloatingActionButton) mapView.findViewById(R.id.addEventFab);
         myFabButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setUpMap();
+                initializePlacePicker();
             }
         });
-
         Toast.makeText(getContext(), R.string.map_gps_warning, Toast.LENGTH_LONG).show();
-
         return mapView;
     }
 
+    /*
+    *   Adds some features on the map once the map becomes ready
+    */
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
@@ -102,82 +119,80 @@ public class BottomBarMap extends Fragment implements OnMapReadyCallback, Google
         updateMyLocation();
     }
 
-    private void setUpMap() {
-        Toast.makeText(getContext(), R.string.choose_location, Toast.LENGTH_LONG).show();
-        myGoogleMap.setOnMapClickListener(this);
-    }
-
-    @Override
-    public void onMapClick(LatLng point) {
-        eventPoint = point;
-        addEvent(eventPoint);
-    }
-
-    private void addEvent(LatLng point) {
-        //myGoogleMap.addMarker(new MarkerOptions().position(point).title("Event"));
+    /*
+    *   Opens PlacePicker
+    */
+    private void initializePlacePicker() {
 
         try {
-            geocoder = new Geocoder(getContext(), Locale.getDefault());
-            List<Address> addresses = geocoder.getFromLocation(point.latitude, point.longitude, 1);
-            if (addresses.isEmpty()) {
-                Log.v("ADDRESS", "Waiting for Location");
-            }
-            else {
-                if (addresses.size() > 0) {
-                    String city = "unknown", knownName = "unknown", state = "unknown", country = "unknown", county = "unknown";
+            PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
+            Intent intent = intentBuilder.build(getActivity());
+            // Start the Intent by requesting a result, identified by a request code.
+            startActivityForResult(intent, REQUEST_PLACE_PICKER);
 
-                     // Gives whole address
-                    for (int i = 0; i < addresses.get(0).getMaxAddressLineIndex(); i++) {
-                        Log.v("ADDRESS",addresses.get(0).getAddressLine(i));
-                    }
-
-                    //extracting address info
-                    if (addresses.get(0).getLocality() != null)  city = addresses.get(0).getLocality();
-                    else
-                        if (addresses.get(0).getSubAdminArea() != null)  city = addresses.get(0).getSubAdminArea();
-
-                    if (addresses.get(0).getFeatureName() != null) knownName = addresses.get(0).getFeatureName();
-                    if (addresses.get(0).getAdminArea() != null) state = addresses.get(0).getAdminArea();
-                    if (addresses.get(0).getCountryName() != null) country = addresses.get(0).getCountryName();
-                    if (addresses.get(0).getSubAdminArea() != null)  county = addresses.get(0).getSubAdminArea();
-
-                    Log.v("ADDRESS", knownName + ", " + county + ", " + city + ", " + state + ", " + country);
-
-                    Log.v("ADDRESS", addresses.get(0).getFeatureName() + ", " + addresses.get(0).getSubAdminArea() +", " + addresses.get(0).getPremises() + ", " + addresses.get(0).getCountryName());
-                    Toast.makeText(getContext(), "Address:- " + addresses.get(0).getFeatureName() + addresses.get(0).getSubAdminArea() + addresses.get(0).getLocality(), Toast.LENGTH_LONG).show();
-                }
-            }
-
-           /* PlacePicker.IntentBuilder ibuilder = new PlacePicker.IntentBuilder();
-            Intent intent = ibuilder.build(getActivity());
-            startActivityForResult(intent, REQUEST_PLACE_PICKER);*/
-        }
-        /*catch (GooglePlayServicesRepairableException e) {
-            GooglePlayServicesUtil
-                    .getErrorDialog(e.getConnectionStatusCode(), getActivity(), 0);
+        } catch (GooglePlayServicesRepairableException e) {
+            GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+            apiAvailability.getErrorDialog(getActivity(), e.getConnectionStatusCode(), PLAY_SERVICES_RESOLUTION_REQUEST).show();
         } catch (GooglePlayServicesNotAvailableException e) {
             Toast.makeText(getActivity(), "Google Play Services is not available.",
                     Toast.LENGTH_LONG)
                     .show();
-        }*/ catch (IOException e) {
-            e.printStackTrace();
         }
-        //Disabling fab after event location is chosen
-        myGoogleMap.setOnMapClickListener(null);
     }
 
-    //For PlacePicker
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent intentData) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_PLACE_PICKER) {
-            if (resultCode == RESULT_OK) {
-                Place selectedPlace = PlacePicker.getPlace(getContext(), intentData);
-                myGoogleMap.addMarker(new MarkerOptions().position(selectedPlace.getLatLng()).title("Event"));
+
+            if (resultCode == getActivity().RESULT_OK) {
+                /* User has picked a place, extract data.
+                   Data is extracted from the returned intent by retrieving a Place object from
+                   the PlacePicker.
+                 */
+                final Place place = PlacePicker.getPlace(getContext(), data);
+
+                /* A Place object contains details about that place, such as its name, address
+                and phone number. Extract the name, address, phone number, place ID and place types.
+                 */
+                final CharSequence place_name = place.getName();
+                final CharSequence address = place.getAddress();
+                final CharSequence phone = place.getPhoneNumber();
+                final String placeId = place.getId();
+
+                //Create Event Dialog Initialization
+                showDialog(place, requestCode);
+
+                //addEvent(place);
+                Log.d("PLACEPICKER", "Place selected: " + placeId + " (" + place_name.toString() + ")");
+
+            } else {
+                Log.d("PLACEPICKER", "Error in PlacePicker ResultCode: " + resultCode);
             }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
         }
-        else {
-            super.onActivityResult(requestCode, resultCode, intentData);
+    }
+
+    private void showDialog(Place place, int requestCode) {
+
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+        if (prev != null) {
+            ft.remove(prev);
         }
+        ft.addToBackStack(null);
+
+        // Create the fragment and show it as a dialog.
+        DialogFragment newDialog = EventCreateFragment.newInstance(place);
+        newDialog.setTargetFragment(this, requestCode);
+        newDialog.show(ft, "dialog");
+    }
+
+    public void addEvent(Place place) {
+
+        myGoogleMap.addMarker(new MarkerOptions().position(place.getLatLng())
+                .title("Event"));
+
     }
 
     private boolean checkMapReady() {
@@ -188,8 +203,10 @@ public class BottomBarMap extends Fragment implements OnMapReadyCallback, Google
         return true;
     }
 
+    /*
+    *   Updates the user's location and zooms in current location on startup
+    */
     private void updateMyLocation() {
-        // TODO: Zoom in my location on start up
 
         if (!checkMapReady()) {
             return;
@@ -198,12 +215,39 @@ public class BottomBarMap extends Fragment implements OnMapReadyCallback, Google
             Log.v("LOCATION", "Permissions GIVEN for My Location");
             myGoogleMap.setMyLocationEnabled(true);
             myGoogleMap.setBuildingsEnabled(true);
+
+            //Zoom in the known location
+            Criteria criteria = new Criteria();
+            Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+            zoomInLocation(location);
+
             return;
-        }
-        else {
+
+        } else {
             PermissionUtils.requestPermission((AppCompatActivity) getActivity(), LOCATION_PERMISSION_REQUEST_CODE,
                     Manifest.permission.ACCESS_FINE_LOCATION, false);
             Log.v("LOCATION", "Permission conditions not satisfied for My Location");
+        }
+    }
+
+    /*
+    *   Zooms in the given location
+     */
+    private void zoomInLocation(Location location){
+        if (location != null) {
+            myGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(location.getLatitude(), location.getLongitude()), 13));
+
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
+                    .zoom(DEFAULT_ZOOM)         // Sets the zoom
+                    .bearing(DEFAULT_BEARING)   // Sets the orientation of the camera to east
+                    .tilt(DEFAULT_TILT)         // Sets the tilt of the camera to 30 degrees
+                    .build();                   // Creates a CameraPosition from the builder
+            myGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
+        else{
+            Log.v("LOCATION", "Location is null");
         }
     }
 
@@ -217,5 +261,23 @@ public class BottomBarMap extends Fragment implements OnMapReadyCallback, Google
             return;
         }
         myGoogleMap.setMyLocationEnabled(true);
+    }
+
+    /*
+    * This method must be implemented if locationUpdates are necessary on regular basis.
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onCancel(DialogInterface dialog) {
+
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+
     }
 }
