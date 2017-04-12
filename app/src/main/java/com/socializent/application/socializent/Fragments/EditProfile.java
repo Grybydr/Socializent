e68a09d06b6743ee1f9ed9814168f90cba2ab1b8
iremherguner <hergunerirem@gmail.com>
2017-04-13 01:02:13 +0300

    edit page e başladım ama daha bitmedi

---
+++ app/src/main/java/com/socializent/application/socializent/Fragments/EditProfile.java
@@ -0,0 +1,15 @@
+package com.socializent.application.socializent.Fragments;
+
+import android.view.View;
+
+/**
+ * Created by Irem on 13.4.2017.
+ */
+
+public class EditProfile {
+
+
+    public void onCheckBoxClicked(View view){
+
+    }
+}
